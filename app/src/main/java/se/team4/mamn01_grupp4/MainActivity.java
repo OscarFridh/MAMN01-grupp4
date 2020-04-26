c1df0fdf7ac97a9d3d2c5f43d7c3dcecef0621c6
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2020-04-26 14:19:33 +0200

    Added empty Android Studio project

---
+++ app/src/main/java/se/team4/mamn01_grupp4/MainActivity.java
@@ -0,0 +1,14 @@
+package se.team4.mamn01_grupp4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
