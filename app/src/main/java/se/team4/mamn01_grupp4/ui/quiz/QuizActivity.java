612726186ca88d4bc48a085739f87132cb2d3513
frykiz <Robotar971>
2020-05-09 15:00:46 +0200

    Scoring and other stuff implemented in QuizActivity

--- app/src/main/java/se/team4/mamn01_grupp4/ui/quiz/QuizActivity.java
+++ app/src/main/java/se/team4/mamn01_grupp4/ui/quiz/QuizActivity.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -24,12 +26,30 @@ public class QuizActivity extends AppCompatActivity {
 
     MediaPlayer player;
     private ImageView playButton;
+    private TextView bonusText;
+    private TextView bonusValue;
     private TextView timeValue;
     private Poi poi;
+    private int bonusScore;
+    FrameLayout container;
     TextView questionView;
     TextView locationView;
     Logger LOGGER;
     private int score = 1;
+
+    CountDownTimer scoreCounter = new CountDownTimer(20000, 2000) {
+
+        public void onTick(long millisUntilFinished) {
+            bonusValue.setText(String.valueOf(millisUntilFinished / 2000));
+            bonusScore = (int) (millisUntilFinished / 1000);
+        }
+
+        public void onFinish() {
+            bonusText.setText("");
+            bonusValue.setText("");
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +63,6 @@ public class QuizActivity extends AppCompatActivity {
             LOGGER.e("Found %s in passed args to quizActivity", arg);
             poi = PoiDb.getDb().get(arg);
 
-
             player = new MediaPlayer();
             try {
                 player.setDataSource(poi.sound.getFileDescriptor(), poi.sound.getStartOffset(), poi.sound.getLength());
@@ -54,7 +73,10 @@ public class QuizActivity extends AppCompatActivity {
         }
 
 
+        container = findViewById(R.id.quiz_window);
         playButton = findViewById(R.id.playButton);
+        bonusText = findViewById(R.id.BonusText);
+        bonusValue = findViewById(R.id.BonusValue);
         timeValue = findViewById(R.id.TimeValue);
         locationView = findViewById(R.id.locationName);
         questionView = findViewById(R.id.question);
@@ -67,13 +89,22 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(player != null){
-                    if(player.isPlaying()){
-                        player.pause();
-                        playButton.setImageResource(R.drawable.ic_play_circle_filled_black_24dp);
-                    } else{
-                        player.start();
-                        playButton.setImageResource(R.drawable.ic_pause_circle_filled_black_24dp);
-                    }
+                    player.start();
+                    playButton.setImageResource(R.drawable.ic_pause_circle_filled_black_24dp);
+                    scoreCounter.start();
+                    new CountDownTimer(60000, 1000) {
+
+                        public void onTick(long millisUntilFinished) {
+                            timeValue.setText(String.valueOf(millisUntilFinished / 1000));
+                        }
+
+                        public void onFinish() {
+                            questionView.setText("You got 0 points!");
+                            container.setBackgroundResource(R.drawable.gradient_red_background);
+                            player.stop();
+                        }
+
+                    }.start();
                 } else{
                     LOGGER.e("Sound file not found");
                 }
@@ -86,6 +117,17 @@ public class QuizActivity extends AppCompatActivity {
 
     }
 
+    private void evaluateResult(boolean ans, int score){
+        if(poi.ans == ans){
+            container.setBackgroundResource(R.drawable.gradient_green_background);
+            questionView.setText("You got " + (score+5) + "points!");
+        } else {
+            questionView.setText("You got 0 points!");
+            container.setBackgroundResource(R.drawable.gradient_red_background);
+        }
+        player.stop();
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -95,6 +137,7 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onStop(){
         super.onStop();
+        player.stop();
     }
 
 }
